2ec7de202b28a106285771b50382042284281c83
zhangyuyuan <zhangyuyuan@szmsd.com>
2022-07-05 11:49:34 +0800

    fix: 修改字体加载逻辑。

--- business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/util/ITextPdfFontUtil.java
+++ business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/util/ITextPdfFontUtil.java
@@ -3,13 +3,19 @@ package com.szmsd.delivery.util;
 import cn.hutool.cache.impl.LRUCache;
 import com.szmsd.common.core.exception.com.CommonException;
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
 public final class ITextPdfFontUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ITextPdfFontUtil.class);
 
     private ITextPdfFontUtil() {
     }
@@ -21,20 +27,41 @@ public final class ITextPdfFontUtil {
         if (fontCache.containsKey(font)) {
             return fontCache.get(font);
         } else {
-            URL fontUrl = ITextPdfFontUtil.class.getClassLoader().getResource(font);
-            UrlResource urlResource = new UrlResource(fontUrl);
+            Resource resource = new ClassPathResource(font);
             InputStream inputStream = null;
             byte[] fontBytes;
-            try {
-                inputStream = urlResource.getInputStream();
-                fontBytes = new byte[inputStream.available()];
-                inputStream.read(fontBytes);
-                fontCache.put(font, fontBytes);
-            } catch (IOException e) {
-                throw new CommonException("400", "加载字体失败，" + font + "，" + e.getMessage());
-            } finally {
-                IOUtils.closeQuietly(inputStream);
+            if (resource.exists()) {
+                ByteArrayOutputStream byteArrayOutputStream = null;
+                try {
+                    inputStream = resource.getInputStream();
+                    byteArrayOutputStream = new ByteArrayOutputStream();
+                    IOUtils.copy(inputStream, byteArrayOutputStream);
+                    fontBytes = byteArrayOutputStream.toByteArray();
+                } catch (IOException e) {
+                    LOGGER.error("加载字体失败," + e.getMessage(), e);
+                    throw new CommonException("400", "加载字体失败，" + font + "，" + e.getMessage());
+                } finally {
+                    IOUtils.closeQuietly(byteArrayOutputStream);
+                    IOUtils.closeQuietly(inputStream);
+                }
+            } else {
+                URL fontUrl = ITextPdfFontUtil.class.getClassLoader().getResource(font);
+                UrlResource urlResource = new UrlResource(fontUrl);
+                ByteArrayOutputStream byteArrayOutputStream = null;
+                try {
+                    inputStream = urlResource.getInputStream();
+                    byteArrayOutputStream = new ByteArrayOutputStream();
+                    IOUtils.copy(inputStream, byteArrayOutputStream);
+                    fontBytes = byteArrayOutputStream.toByteArray();
+                } catch (IOException e) {
+                    LOGGER.error("加载字体失败," + e.getMessage(), e);
+                    throw new CommonException("400", "加载字体失败，" + font + "，" + e.getMessage());
+                } finally {
+                    IOUtils.closeQuietly(byteArrayOutputStream);
+                    IOUtils.closeQuietly(inputStream);
+                }
             }
+            fontCache.put(font, fontBytes);
             return fontBytes;
         }
     }
