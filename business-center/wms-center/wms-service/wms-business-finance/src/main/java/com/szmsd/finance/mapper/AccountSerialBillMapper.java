c7f986d95191d5e24ec13c3977c1042ebadc4bcb
jiangjun <jiangjun@szmsd.com>
2022-07-25 12:24:12 +0800

    修改mybatis 财务业务明细
    (cherry picked from commit 32df757102ad67035c3c3cd12a650b2ebeb93a64)

--- business-center/wms-center/wms-service/wms-business-finance/src/main/java/com/szmsd/finance/mapper/AccountSerialBillMapper.java
+++ business-center/wms-center/wms-service/wms-business-finance/src/main/java/com/szmsd/finance/mapper/AccountSerialBillMapper.java
@@ -5,12 +5,13 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.toolkit.Constants;
 import com.szmsd.common.datascope.annotation.DataScope;
 import com.szmsd.finance.domain.AccountSerialBill;
+import com.szmsd.finance.dto.AccountSerialBillDTO;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface AccountSerialBillMapper extends BaseMapper<AccountSerialBill> {
 
-    @DataScope("cus_code")
-    List<AccountSerialBill> selectPageList(@Param(Constants.WRAPPER) LambdaQueryWrapper queryWrapper);
+//    @DataScope("cus_code")
+    List<AccountSerialBill> selectPageList(AccountSerialBillDTO dto);
 }
