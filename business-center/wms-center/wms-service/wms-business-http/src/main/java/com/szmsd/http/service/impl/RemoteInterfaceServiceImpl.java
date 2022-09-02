c6fbf4f8e05cc1c20f0b2b02b1d70ee93339d5a6
jiangjun <jiangjun@szmsd.com>
2022-09-02 15:38:59 +0800

    调用第三方接口初步代码搭建

--- business-center/wms-center/wms-service/wms-business-http/src/main/java/com/szmsd/http/service/impl/RemoteInterfaceServiceImpl.java
+++ business-center/wms-center/wms-service/wms-business-http/src/main/java/com/szmsd/http/service/impl/RemoteInterfaceServiceImpl.java
@@ -174,6 +174,12 @@ public class RemoteInterfaceServiceImpl implements RemoteInterfaceService {
                     requestHeaders.put("Authorization", Authorization);
             }
 
+            //国外接口地址
+            if (domain.equals("https://ws-pf.qeops.eu:8445")){
+                    Authorization = "Bearer " + "qps_auth_u96vL6IVHm2NwseYO0oHGk5Y2wYWQYn6adad70";
+                    requestHeaders.put("Authorization", Authorization);
+            }
+
             // 二进制
             Boolean binary = dto.getBinary();
             if (null == binary) {
