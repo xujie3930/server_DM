d26f54307482d155e977728b96b87b513ecb4ee5
jiangjun <jiangjun@szmsd.com>
2022-09-06 18:38:28 +0800

    查件列表新增，修改一下条件，返回函数集

--- business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/service/impl/DelOutboundServiceImpl.java
+++ business-center/wms-center/wms-service/wms-business-delivery/src/main/java/com/szmsd/delivery/service/impl/DelOutboundServiceImpl.java
@@ -201,12 +201,17 @@ public class DelOutboundServiceImpl extends ServiceImpl<DelOutboundMapper, DelOu
         LambdaQueryWrapper<DelOutbound> queryWrapper = Wrappers.lambdaQuery();
         queryWrapper.isNotNull(DelOutbound::getShipmentsTime);
         queryWrapper.isNotNull(DelOutbound::getTrackingTime);
-        queryWrapper.eq(DelOutbound::getOrderNo, orderNo).or().eq(DelOutbound::getRefNo,orderNo).or().eq(DelOutbound::getTrackingNo,orderNo);
+//        queryWrapper.eq(DelOutbound::getOrderNo, orderNo).or().eq(DelOutbound::getRefNo,orderNo).or().eq(DelOutbound::getTrackingNo,orderNo);
 //        queryWrapper.and((wrapper)->{
 //
 //
 //
 //        });
+
+        queryWrapper.and(wrapper ->
+                wrapper.eq(DelOutbound::getOrderNo, orderNo).or().eq(DelOutbound::getRefNo,orderNo).or().eq(DelOutbound::getTrackingNo,orderNo)
+        );
+
 
         queryWrapper.last("LIMIT 1");
         DelOutbound delOutbound = super.getOne(queryWrapper);
