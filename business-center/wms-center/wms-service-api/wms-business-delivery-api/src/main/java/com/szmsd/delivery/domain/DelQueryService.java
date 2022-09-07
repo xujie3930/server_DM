670c8d48bbd44447720f72d7ba941784b73675cd
jiangjun <jiangjun@szmsd.com>
2022-09-07 10:18:47 +0800

    查询存入判断条件的天数到表中

--- business-center/wms-center/wms-service-api/wms-business-delivery-api/src/main/java/com/szmsd/delivery/domain/DelQueryService.java
+++ business-center/wms-center/wms-service-api/wms-business-delivery-api/src/main/java/com/szmsd/delivery/domain/DelQueryService.java
@@ -108,9 +108,24 @@ public class DelQueryService extends BaseEntity {
     @Excel(name = "状态名称")
     private String stateName;
 
+    @ApiModelProperty(value = "发货天数")
+    private Long delDays;
+
+    @ApiModelProperty(value = "轨迹停留天数")
+    private Long trackingDays;
+
     @ApiModelProperty(value = "查件标识(0是红色,1是绿色)")
-    @TableField(exist = false)
     private Long checkFlag;
+
+    @ApiModelProperty(value = "允查发货天数")
+    private Long queryseShipmentDays;
+
+    @ApiModelProperty(value = "允查轨迹停留天数")
+    private Long querysetrackStayDays;
+
+//    @ApiModelProperty(value = "查件标识(0是红色,1是绿色)")
+//    //@TableField(exist = false)
+//    private Long checkFlag;
 
 
 }
