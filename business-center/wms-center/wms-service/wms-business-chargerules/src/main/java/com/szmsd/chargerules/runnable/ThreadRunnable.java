014575898cfb9690377ee9eb1a44535334a4e373
jiangjun <jiangjun@szmsd.com>
2022-09-13 15:51:23 +0800

    定时任务修改报错

--- business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/runnable/ThreadRunnable.java
+++ business-center/wms-center/wms-service/wms-business-chargerules/src/main/java/com/szmsd/chargerules/runnable/ThreadRunnable.java
@@ -68,7 +68,7 @@ public class ThreadRunnable {
 //    @Scheduled(cron = "0/60 * * * * *")
     // @Scheduled(cron = "0 10 0/1 * * ?") 每小时执行一次
     //每周日0 0 20 ? * 1"
-    @Scheduled(cron = "0 0 20 * * ? *")
+    @Scheduled(cron = "0 0 20 * * ?")
     public void executeWarehouse() {
         log.info("executeWarehouse() start...");
         RLock lock = redissonClient.getLock("executeOperation");
