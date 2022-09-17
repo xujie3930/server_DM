8bd134c3f0d2686b301b02d62549c3e64f77eb77
weixiaofeng <260008747@qq.com>
2022-09-18 00:39:57 +0800

    accountBalance3

--- business-center/wms-center/wms-service/wms-business-finance/src/main/java/com/szmsd/finance/factory/BalanceFreezeFactory.java
+++ business-center/wms-center/wms-service/wms-business-finance/src/main/java/com/szmsd/finance/factory/BalanceFreezeFactory.java
@@ -68,7 +68,14 @@ public class BalanceFreezeFactory extends AbstractPayFactory {
                 final String currencyCode = dto.getCurrencyCode();
 
                 log.info("【updateBalance】 1 开始查询该用户对应币别的{}余额,客户ID：{}",currencyCode,dto.getCusCode());
-                final BalanceDTO balance = getBalance(dto.getCusCode(), dto.getCurrencyCode());
+                //final BalanceDTO balance = getBalance(dto.getCusCode(), dto.getCurrencyCode());
+
+                QueryWrapper<AccountBalance> queryWrapper = new QueryWrapper<>();
+                queryWrapper.eq("cus_code", dto.getCusCode());
+                queryWrapper.eq("currency_code", currencyCode);
+                AccountBalance accountBalance = accountBalanceMapper.selectOne(queryWrapper);
+                BalanceDTO balance = new BalanceDTO(accountBalance.getCurrentBalance(), accountBalance.getFreezeBalance(), accountBalance.getTotalBalance());
+
                 log.info("【updateBalance】 2 {} 可用余额：{}，冻结余额：{}，总余额：{},余额剩余：{} ",currencyCode,balance.getCurrentBalance(),balance.getFreezeBalance(),balance.getTotalBalance(),JSONObject.toJSONString(balance));
                 //蒋俊看财务
                 final Boolean checkFlag = checkAndSetBalance(balance, dto);
@@ -103,12 +110,12 @@ public class BalanceFreezeFactory extends AbstractPayFactory {
 
                 //final BalanceDTO balancesel = getBalance(dto.getCusCode(), dto.getCurrencyCode());
 
-                QueryWrapper<AccountBalance> queryWrapper = new QueryWrapper<>();
-                queryWrapper.eq("cus_code", dto.getCusCode());
-                queryWrapper.eq("currency_code", currencyCode);
-                AccountBalance accountBalance = accountBalanceMapper.selectOne(queryWrapper);
+                QueryWrapper<AccountBalance> queryWrapper1 = new QueryWrapper<>();
+                queryWrapper1.eq("cus_code", dto.getCusCode());
+                queryWrapper1.eq("currency_code", currencyCode);
+                AccountBalance accountBalance3 = accountBalanceMapper.selectOne(queryWrapper1);
 
-                log.info("【updateBalance】 4.1 {} setBalance后可用余额：{}，冻结余额：{}，总余额：{},余额剩余：{} ",currencyCode,accountBalance.getCurrentBalance(),accountBalance.getFreezeBalance(),accountBalance.getTotalBalance(),JSONObject.toJSONString(accountBalance));
+                log.info("【updateBalance】 4.1 {} setBalance后可用余额：{}，冻结余额：{}，总余额：{},余额剩余：{} ",currencyCode,accountBalance3.getCurrentBalance(),accountBalance3.getFreezeBalance(),accountBalance3.getTotalBalance(),JSONObject.toJSONString(accountBalance3));
                 log.info("【updateBalance】 5");
                 recordOpLogAsync(dto, balance.getCurrentBalance());
                 recordDetailLogAsync(dto, balance);
