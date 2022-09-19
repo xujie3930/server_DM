bd58d7fe38d92d592e59fc55ca79010703ad8a0f
jiangjun <jiangjun@szmsd.com>
2022-09-19 11:16:43 +0800

    百度翻译  公用api

---
+++ business-center/com-center/com-service/business-basic/src/main/java/com/szmsd/bas/baiduTranslate/Main.java
@@ -0,0 +1,28 @@
+package com.szmsd.bas.baiduTranslate;
+
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    // 在平台申请的APP_ID 详见 http://api.fanyi.baidu.com/api/trans/product/desktop?req=developer
+    private static final String APP_ID = "20220919001347550";
+    private static final String SECURITY_KEY = "xTFW4pP6aatqfTAkfYCR";
+
+//    public static void main(String[] args) {
+//        TransApi api = new TransApi(APP_ID, SECURITY_KEY);
+//
+//        String query = "有问题 报错了，快给我处理";
+//        //System.out.println(api.getTransResult(query, "auto", "en"));
+//        JSONObject jsonObject= JSON.parseObject(api.getTransResult(query, "auto", "en"));
+//        List<Map> list= (List<Map>) jsonObject.get("trans_result");
+//        //System.out.println(list);
+//        String dst=String.valueOf(list.get(0).get("dst"));
+//        System.out.println(dst);
+//    }
+
+}
