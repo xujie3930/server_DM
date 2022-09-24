2fdad81c9764a6fc92ba274c8a2e7f4be26daeb7
jiangjun <jiangjun@szmsd.com>
2022-09-24 10:06:53 +0800

    解决代码冲突

--- business-center/wms-center/wms-service/wms-business-putinstorage/src/main/java/com/szmsd/putinstorage/service/impl/InboundReceiptRecordServiceImpl.java
+++ business-center/wms-center/wms-service/wms-business-putinstorage/src/main/java/com/szmsd/putinstorage/service/impl/InboundReceiptRecordServiceImpl.java
@@ -11,20 +11,64 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 @Service
 @Slf4j
 public class InboundReceiptRecordServiceImpl extends ServiceImpl<InboundReceiptRecordMapper, InboundReceiptRecord> implements IInboundReceiptRecordService {
 
     @Override
-    public List<InboundReceiptRecord> selectList(InboundReceiptRecordQueryDTO queryDTO) {
+    public List<InboundReceiptRecord> selectList(InboundReceiptRecordQueryDTO queryDTO,String le) {
         LambdaQueryWrapper<InboundReceiptRecord> queryWrapper = Wrappers.lambdaQuery();
         queryWrapper.eq(StringUtils.isNotEmpty(queryDTO.getWarehouseNo()), InboundReceiptRecord::getWarehouseNo, queryDTO.getWarehouseNo());
         queryWrapper.eq(StringUtils.isNotEmpty(queryDTO.getType()), InboundReceiptRecord::getType, queryDTO.getType());
         queryWrapper.ne(InboundReceiptRecord::getType, "修改");
         queryWrapper.orderByDesc(InboundReceiptRecord::getCreateTime);
-        return baseMapper.selectList(queryWrapper);
+        List<InboundReceiptRecord>  inboundReceiptRecordList=baseMapper.selectList(queryWrapper);
+        String li="上架数量";
+        if (le.equals("en")) {
+            inboundReceiptRecordList.forEach(x -> {
+                if (x.getRemark().contains(li)) {
+                    //拼接*,把敏感词替换成Quantity put on shelves
+                    String s = "Quantity put on shelves";
+//                for (int j = 0; j <li.length(); j++) {
+//                    s+="Quantity put on shelves";
+//                }
+                    x.setRemark(x.getRemark().replace(li, s));
+                }
+            });
+        }
+        return inboundReceiptRecordList;
     }
+
+//    public static void main(String[] args) {
+//        List li =new ArrayList();
+//        li.add("易烊千玺");
+//        li.add("朱一龙");
+//        li.add("周雨彤");
+//        li.add("张子枫");
+//
+//        Scanner sc  = new Scanner(System.in);
+//        System.out.println("请输入评论内容:");
+//        String content = sc.nextLine();
+//
+//        for (int i = 0; i < li.size(); i++) {
+//            String ci = (String) li.get(i);//拿到敏感词，强转
+//            if(content.contains(ci))
+//            {
+//                //拼接*,把敏感词替换成*
+//                String s = "";
+//                for (int j = 0; j <ci.length(); j++) {
+//                    s+="*";
+//                }
+//                content=content.replace(ci,s);
+//            }
+//        }
+//
+//        System.out.println(content);
+//    }
+
 }
 
