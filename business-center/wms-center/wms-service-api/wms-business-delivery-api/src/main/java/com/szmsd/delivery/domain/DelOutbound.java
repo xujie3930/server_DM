68118840e88ef3adad6b4fa806607419c94c07e2
jiangjun <jiangjun@szmsd.com>
2022-10-27 10:33:32 +0800

    提交出库同事的代码

--- business-center/wms-center/wms-service-api/wms-business-delivery-api/src/main/java/com/szmsd/delivery/domain/DelOutbound.java
+++ business-center/wms-center/wms-service-api/wms-business-delivery-api/src/main/java/com/szmsd/delivery/domain/DelOutbound.java
@@ -68,6 +68,9 @@ public class DelOutbound extends BaseEntity {
     @ApiModelProperty(value = "发货规则（也就是物流承运商，必须填写指定值，例如Fedex, USPS等，相同代表一起交货。）")
     private String shipmentRule;
 
+    @ApiModelProperty(value = "发货规则名称（也就是物流承运商，必须填写指定值，例如Fedex, USPS等，相同代表一起交货。）")
+    private String shipmentRuleName;
+
     @ApiModelProperty(value = "发货服务名称")
     private String shipmentService;
 
