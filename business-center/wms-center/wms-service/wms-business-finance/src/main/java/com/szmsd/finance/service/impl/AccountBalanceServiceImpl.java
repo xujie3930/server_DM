a92b95505c5fff93354e8f6602f9139767796602
xujie3930 <17329915422@163.com>
2022-11-04 16:42:59 +0800

    增加错误日志操作

--- business-center/wms-center/wms-service/wms-business-finance/src/main/java/com/szmsd/finance/service/impl/AccountBalanceServiceImpl.java
+++ business-center/wms-center/wms-service/wms-business-finance/src/main/java/com/szmsd/finance/service/impl/AccountBalanceServiceImpl.java
@@ -600,7 +600,11 @@ public class AccountBalanceServiceImpl implements IAccountBalanceService {
         {
             log.info(LogUtil.format(cfbDTO, "费用冻结", "操作费用表"));
             //TODO：这里通过异步线程去插入chargerules模块的cha_log表，并不能保证插入成功
-            this.addOptLogAsync(dto);
+            try {
+                this.addOptLogAsync(dto);
+            } catch (Exception e) {
+                log.error("创建cha_log表失败:{}", e.toString());
+            }
         }
         return flag ? R.ok() : R.failed(Strings.nullToEmpty(dto.getCurrencyName()) + "The available balance of the account is insufficient for freezing");
     }
