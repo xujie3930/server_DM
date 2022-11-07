4347cb9494eb67b5e1133847ab9f50c35d9f026a
xujie3930 <17329915422@163.com>
2022-11-07 17:37:41 +0800

    日志增加线程id

---
+++ szmsd-framework/szmsd-common/szmsd-common-core/src/main/java/com/szmsd/common/core/interceptor/LogInterceptor.java
@@ -0,0 +1,44 @@
+package com.szmsd.common.core.interceptor;
+
+/**
+ * @author xujie
+ * @description 全局trackId
+ * @create 2022-11-07 17:25
+ **/
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+/**
+ * 日志拦截器
+ */
+@Slf4j
+@Component
+public class LogInterceptor implements HandlerInterceptor {
+    // 会话ID
+    private final static String SESSION_ID = "sessionId";
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) {
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+        MDC.put(SESSION_ID, uuid);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) {
+        MDC.remove(SESSION_ID);
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
+    }
+}
---
+++ szmsd-framework/szmsd-common/szmsd-common-core/src/main/java/com/szmsd/common/core/interceptor/WebMvcConfigurer.java
@@ -0,0 +1,24 @@
+package com.szmsd.common.core.interceptor;
+
+/**
+ * @author xujie
+ * @description WebMvcConfigurer
+ * @create 2022-11-07 17:27
+ **/
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
+    @Autowired
+    private LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor);
+        super.addInterceptors(registry);
+    }
+}
