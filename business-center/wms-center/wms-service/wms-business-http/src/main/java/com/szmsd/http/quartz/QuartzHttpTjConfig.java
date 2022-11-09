76b33e845aef1e8dca500d9ed60f6b80a0256f33
jiangjun <jiangjun@szmsd.com>
2022-11-09 17:57:06 +0800

    修改两个定时任务

--- business-center/wms-center/wms-service/wms-business-http/src/main/java/com/szmsd/http/quartz/QuartzHttpTjConfig.java
+++ business-center/wms-center/wms-service/wms-business-http/src/main/java/com/szmsd/http/quartz/QuartzHttpTjConfig.java
@@ -56,7 +56,7 @@ public class QuartzHttpTjConfig {
         //0 0 */1 * * ? 一个小时
         return TriggerBuilder.newTrigger().forJob(CustomPricesgradeJob())
                 .withIdentity("CustomPricesgradeJob")
-                .withSchedule(CronScheduleBuilder.cronSchedule("0 */50 * * * ?"))
+                .withSchedule(CronScheduleBuilder.cronSchedule("0 0 1 ? * MON"))
                 .build();
     }
 
