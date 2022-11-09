8dab95dd547413fe12f22ab78ad96d41cac35a57
jiangjun <jiangjun@szmsd.com>
2022-11-09 11:27:24 +0800

    入库创建入库单去掉事务

--- business-center/wms-center/wms-service/wms-business-putinstorage/src/main/java/com/szmsd/putinstorage/service/IInboundReceiptService.java
+++ business-center/wms-center/wms-service/wms-business-putinstorage/src/main/java/com/szmsd/putinstorage/service/IInboundReceiptService.java
@@ -43,6 +43,7 @@ public interface IInboundReceiptService extends IService<InboundReceipt> {
 
     void review(InboundReceiptReviewDTO inboundReceiptReviewDTO);
 
+
     void delete(String warehouseNo);
 
     List<InboundReceiptExportVO> selectExport(InboundReceiptQueryDTO queryDTO);
