24a5b72b866d7787768bfa38d8cfda1fa47c2426
jiangjun <jiangjun@szmsd.com>
2022-11-11 12:00:14 +0800

    cod接口拉取数据

--- business-center/wms-center/wms-service/wms-business-http/src/main/java/com/szmsd/http/service/impl/DiscountServiceImpl.java
+++ business-center/wms-center/wms-service/wms-business-http/src/main/java/com/szmsd/http/service/impl/DiscountServiceImpl.java
@@ -2,25 +2,37 @@ package com.szmsd.http.service.impl;
 
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
 import com.szmsd.common.core.domain.R;
 import com.szmsd.common.core.utils.HttpResponseBody;
 import com.szmsd.common.core.utils.StringUtils;
 import com.szmsd.common.core.web.page.PageVO;
 import com.szmsd.http.config.HttpConfig;
+import com.szmsd.http.domain.BasCodExternal;
+import com.szmsd.http.dto.HttpRequestDto;
 import com.szmsd.http.dto.OperationRecordDto;
 import com.szmsd.http.dto.discount.*;
+import com.szmsd.http.enums.DomainEnum;
+import com.szmsd.http.mapper.BasCodExternalMapper;
 import com.szmsd.http.service.IHttpDiscountService;
+import com.szmsd.http.service.RemoteInterfaceService;
 import com.szmsd.http.service.http.SaaSPricedRequest;
 import com.szmsd.http.util.HttpResponseVOUtils;
 import com.szmsd.http.dto.discount.DiscountPage;
+import com.szmsd.http.vo.HttpResponseVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -29,6 +41,12 @@ public class DiscountServiceImpl extends SaaSPricedRequest implements IHttpDisco
 
     @Value("${thread.carrierTimes}")
     private int carrierTimes;
+
+    @Autowired
+    private RemoteInterfaceService remoteInterfaceService;
+
+    @Autowired
+    private BasCodExternalMapper basCodExternalMapper;
 
     public DiscountServiceImpl(HttpConfig httpConfig) {
         super(httpConfig);
@@ -169,5 +187,43 @@ public class DiscountServiceImpl extends SaaSPricedRequest implements IHttpDisco
     @Override
     public R<OperationRecordDto> operationRecord(String id) {
         return HttpResponseVOUtils.transformation(httpGetBodyes("", "discount.operationRecord", null, id), OperationRecordDto.class);
+    }
+
+    @Override
+    public R insertBasCodExternal() {
+        try {
+            HttpRequestDto httpRequestDto = new HttpRequestDto();
+            httpRequestDto.setMethod(HttpMethod.GET);
+            basCodExternalMapper.deleteByPrimaryKey();
+            int i=1;
+            while (i>0){
+                String url = DomainEnum.Ck1OpenAPIDomain.wrapper("/v1/Bills/ExchangeRate?PageSize=200&PageIndex="+i+"");
+                i++;
+                httpRequestDto.setUri(url);
+                //httpRequestDto.setBody(tpieceDto);
+                HttpResponseVO httpResponseVO = remoteInterfaceService.rmi(httpRequestDto);
+                Object o=httpResponseVO.getBody();
+                Map map4 = JSONObject.parseObject(String.valueOf(o), Map.class);
+
+                List<BasCodExternal> basCodExternal = JSONArray.parseArray(map4.get("Data").toString(),BasCodExternal.class);
+                if (basCodExternal.size()==0){
+                    break;
+                }
+                basCodExternal.forEach(x->{
+                    String updatedTime=x.getUpdatedTime();
+                    String updatedTimes=updatedTime.substring(6, 19);
+                    Long updatedTimeslong=Long.valueOf(updatedTimes);
+                    Date date = new Date();
+                    date.setTime(updatedTimeslong);
+                    x.setUpdatedTimes(date);
+                    basCodExternalMapper.insertSelective(x);
+                });
+            }
+            return R.ok();
+        }catch (Exception e){
+            e.printStackTrace();
+           return R.failed("");
+        }
+
     }
 }
