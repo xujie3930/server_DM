331cabbfdbfe2933cc8c804671b6fc567f9982ce
weixiaofeng <260008747@qq.com>
2022-11-17 15:50:40 +0800

    在线充值回调调整

--- business-center/wms-center/wms-service/wms-business-finance/src/main/java/com/szmsd/finance/controller/AccountBalanceHelipayController.java
+++ business-center/wms-center/wms-service/wms-business-finance/src/main/java/com/szmsd/finance/controller/AccountBalanceHelipayController.java
@@ -1,5 +1,6 @@
 package com.szmsd.finance.controller;
 
+import cn.hutool.http.HttpRequest;
 import com.helipay.app.trx.facade.response.pay.APPScanPayResponseForm;
 import com.helipay.component.facade.HeliRequest;
 import com.szmsd.common.core.domain.R;
@@ -10,6 +11,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Api(tags = {"账户余额-合利宝"})
@@ -29,14 +31,40 @@ public class AccountBalanceHelipayController {
 
     @ApiOperation(value = "/支付回调")
     @GetMapping("/pay-callback")
-    public String payallback(HeliRequest heliRequest){
+    public String payallback(HttpServletRequest request){
+
+        String content = request.getParameter("content");
+        String sign = request.getParameter("sign");
+        String merchantNo = request.getParameter("merchantNo");
+        String productCode = request.getParameter("productCode");
+        String orderNo = request.getParameter("orderNo");
+
+        HeliRequest heliRequest = new HeliRequest();
+        heliRequest.setContent(content);
+        heliRequest.setSign(sign);
+        heliRequest.setMerchantNo(merchantNo);
+        heliRequest.setProductCode(productCode);
+        heliRequest.setOrderNo(orderNo);
 
         return payService.payCallback(heliRequest);
     }
 
     @ApiOperation(value = "/支付回调")
     @PostMapping("/pay-callback")
-    public String payCallback(@RequestBody HeliRequest heliRequest){
+    public String payCallback(HttpServletRequest request){
+
+        String content = request.getParameter("content");
+        String sign = request.getParameter("sign");
+        String merchantNo = request.getParameter("merchantNo");
+        String productCode = request.getParameter("productCode");
+        String orderNo = request.getParameter("orderNo");
+
+        HeliRequest heliRequest = new HeliRequest();
+        heliRequest.setContent(content);
+        heliRequest.setSign(sign);
+        heliRequest.setMerchantNo(merchantNo);
+        heliRequest.setProductCode(productCode);
+        heliRequest.setOrderNo(orderNo);
 
         return payService.payCallback(heliRequest);
     }
