148d5a8ffc71669fa7010d79db8dc40efe39eb25
jiangjun <jiangjun@szmsd.com>
2022-11-21 21:44:04 +0800

    修改国外正式环境token

--- business-center/wms-center/wms-service/wms-business-http/src/main/java/com/szmsd/http/service/impl/RemoteInterfaceServiceImpl.java
+++ business-center/wms-center/wms-service/wms-business-http/src/main/java/com/szmsd/http/service/impl/RemoteInterfaceServiceImpl.java
@@ -170,7 +170,7 @@ public class RemoteInterfaceServiceImpl implements RemoteInterfaceService {
 
             //国外接口地址
             if (domain.equals("https://ws-pf.qeops.eu:8445")){
-                    Authorization = "Bearer " + "qps_auth_u96vL6IVHm2NwseYO0oHGk5Y2wYWQYn6adad70";
+                    Authorization = "Bearer " + "da68c36158f045c1b817fe4421f2f05e";
                     requestHeaders.put("Authorization", Authorization);
             }
 
