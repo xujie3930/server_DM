7552d89fc6c3befa677c0d8551d4493a7140669f
jiangjun <jiangjun@szmsd.com>
2022-12-02 15:04:04 +0800

    登录修改验证码和增加sku查询打印日志

---
+++ szmsd-framework/szmsd-auth/src/main/java/com/szmsd/auth/config/KaptchaConfig.java
@@ -0,0 +1,55 @@
+package com.szmsd.auth.config;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+import static com.google.code.kaptcha.Constants.KAPTCHA_OBSCURIFICATOR_IMPL;
+import static com.google.code.kaptcha.Constants.KAPTCHA_TEXTPRODUCER_FONT_NAMES;
+
+/**
+ * @author jiangjun
+ * @version 1.0
+ * @description
+ * @date 2022/12/02 17:09
+ */
+@Configuration
+public class KaptchaConfig {
+    @Bean(name="captchaProducer")
+    public DefaultKaptcha getDefaultKaptcha(){
+        DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
+        Properties properties = new Properties();
+        // 图片边框
+        properties.setProperty("kaptcha.border", "yes");
+        // 边框颜色
+        properties.setProperty("kaptcha.border.color", "105,179,90");
+        // 字体颜色
+        properties.setProperty("kaptcha.textproducer.font.color", "blue");
+        // 图片宽
+        properties.setProperty("kaptcha.image.width", "150");
+        // 图片高
+        properties.setProperty("kaptcha.image.height", "60");
+        // 字体大小
+        properties.setProperty("kaptcha.textproducer.font.size", "38");
+        // session key
+        properties.setProperty("kaptcha.session.key", "code");
+        // 验证码长度
+        properties.setProperty("kaptcha.textproducer.char.length", "4");
+        // 字体
+        properties.setProperty("kaptcha.textproducer.font.names", "宋体,楷体,微软雅黑");
+        // 验证码文本字体样式 默认为new Font("Arial", 1, fontSize), new Font("Courier", 1, fontSize)
+        properties.setProperty(KAPTCHA_TEXTPRODUCER_FONT_NAMES, "Arial,Courier");
+        // 图片样式 水纹com.google.code.kaptcha.impl.WaterRipple  鱼眼com.google.code.kaptcha.impl.FishEyeGimpy 阴影com.google.code.kaptcha.impl.ShadowGimpy
+        properties.setProperty(KAPTCHA_OBSCURIFICATOR_IMPL, "com.google.code.kaptcha.impl.ShadowGimpy");
+
+
+
+        Config config = new Config(properties);
+        defaultKaptcha.setConfig(config);
+
+        return defaultKaptcha;
+    }
+}
