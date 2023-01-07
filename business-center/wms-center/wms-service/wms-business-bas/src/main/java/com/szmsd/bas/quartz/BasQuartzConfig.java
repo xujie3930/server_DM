992316d85696af89182fb60c772f8ebed470cdd1
jiangjun <jiangjun@szmsd.com>
2023-01-07 11:42:35 +0800

    定时任务执行redis中的仓库更新
    (cherry picked from commit 1c10727d64df3b09a66922ecd581f7b916b29979)

--- business-center/wms-center/wms-service/wms-business-bas/src/main/java/com/szmsd/bas/quartz/BasQuartzConfig.java
+++ business-center/wms-center/wms-service/wms-business-bas/src/main/java/com/szmsd/bas/quartz/BasQuartzConfig.java
@@ -23,12 +23,32 @@ public class BasQuartzConfig {
 
 
 
+
+    @Bean
+    public JobDetail InitRedisCacheJob() {
+        return JobBuilder.newJob(InitRedisCacheJob.class).withIdentity("InitRedisCacheJob").storeDurably().build();
+    }
+
+
+
+
+
     @Bean
     public Trigger EmailJobTrigger() {
-        //0/3 * * * * ? 3秒测试 0 0 12 * * ?
+        //0/3 * * * * ? 3秒测试 "0 0 12 * * ?"
         return TriggerBuilder.newTrigger().forJob(EmailJob())
                 .withIdentity("EmailJob")
                 .withSchedule(CronScheduleBuilder.cronSchedule("0 0 12 * * ?"))
+                .build();
+    }
+
+
+    @Bean
+    public Trigger InitRedisCacheJobTrigger() {
+        //0/3 * * * * ? 3秒测试 "0 0 12 * * ?"，"0 */50 * * * ?" 五十分钟一次
+        return TriggerBuilder.newTrigger().forJob(InitRedisCacheJob())
+                .withIdentity("InitRedisCacheJob")
+                .withSchedule(CronScheduleBuilder.cronSchedule("0 */10 * * * ?"))
                 .build();
     }
 
