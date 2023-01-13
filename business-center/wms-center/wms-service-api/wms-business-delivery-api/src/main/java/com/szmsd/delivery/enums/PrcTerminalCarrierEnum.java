11bc2484be08bb2056913269131eff79b2811db9
weixiaofeng <260008747@qq.com>
2023-01-13 20:33:09 +0800

    CHOUKOU1

---
+++ business-center/wms-center/wms-service-api/wms-business-delivery-api/src/main/java/com/szmsd/delivery/enums/PrcTerminalCarrierEnum.java
@@ -0,0 +1,24 @@
+package com.szmsd.delivery.enums;
+
+public enum PrcTerminalCarrierEnum {
+
+    CK1("CK1", "出口易订单"),
+    CHOUKOU1("CHOUKOU1","")
+    ;
+
+    private final String code;
+    private final String name;
+
+    PrcTerminalCarrierEnum(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
